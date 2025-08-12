db62010b661a80b8f7cefa976db117ac1d95a724
itsokto <36706257+itsokto@users.noreply.github.com>
2025-08-12 03:40:56 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Run VPN if VPN_ENABLE is 1
-if [[ $VPN_ENABLE -eq 1 ]];then
+if [ "$VPN_ENABLE" -eq 1 ]; then
   echo "startup/vpn: configuring vpn client."
   # template out all the config files using env vars
-  sed -i 's/right=.*/right='$VPN_SERVER'/' /etc/ipsec.conf
-  echo ': PSK "'$VPN_PSK'"' > /etc/ipsec.secrets
-  sed -i 's/lns = .*/lns = '$VPN_SERVER'/' /etc/xl2tpd/xl2tpd.conf
-  sed -i 's/name .*/name '$VPN_USERNAME'/' /etc/ppp/options.l2tpd.client
-  sed -i 's/password .*/password '$VPN_PASSWORD'/' /etc/ppp/options.l2tpd.client
+  sed -i 's/right=.*/right='"$VPN_SERVER"'/' /etc/ipsec.conf
+  echo ': PSK "'"$VPN_PSK"'"' > /etc/ipsec.secrets
+  sed -i 's/lns = .*/lns = '"$VPN_SERVER"'/' /etc/xl2tpd/xl2tpd.conf
+  sed -i 's/name .*/name '"$VPN_USERNAME"'/' /etc/ppp/options.l2tpd.client
+  sed -i 's/password .*/password '"$VPN_PASSWORD"'/' /etc/ppp/options.l2tpd.client
 
   # startup ipsec tunnel
   ipsec initnss
@@ -26,23 +26,23 @@ if [[ $VPN_ENABLE -eq 1 ]];then
 
 
   # startup xl2tpd ppp daemon then send it a connect command
-  (sleep 7 \
-    && echo "startup/vpn: send connect command to vpn client." \
-    && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
-  exec /reconnector.sh &
   echo "startup/vpn: start vpn client daemon."
-  exec /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D &
+  /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D &
+  sleep 7
+  echo "startup/vpn: send connect command to vpn client."
+  echo "c myVPN" > /var/run/xl2tpd/l2tp-control
+  exec /reconnector.sh &
 else
   echo "startup/vpn: Ignore vpn client."
 fi
 
-# Run socks5 server after 10 Seconds if SCOKS5_ENABLE is 1
-if [[ $SCOKS5_ENABLE -eq 1 ]];then
+# Run socks5 server after 10 Seconds if SOCKS5_ENABLE is 1
+if [ "$SOCKS5_ENABLE" -eq 1 ]; then
   echo "startup/socks5: waiting for ppp0"
   (while ! route | grep ppp0 > /dev/null; do sleep 1; done \
-    && echo "startup/socks5: Socks5 will start in $SCOKS5_START_DELAY seconds" \
-    && sleep $SCOKS5_START_DELAY \
-    && sockd -N $SCOKS5_FORKS) &
+    && echo "startup/socks5: Socks5 will start in $SOCKS5_START_DELAY seconds" \
+    && sleep "$SOCKS5_START_DELAY" \
+    && sockd -N "$SOCKS5_FORKS") &
 else
   echo "startup/socks5: Ignore socks5 server."
 fi
